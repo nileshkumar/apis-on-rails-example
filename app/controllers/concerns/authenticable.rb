88f7218b8bcfee2bc5b2a1aba5564ce0d56e2874
Nithin Bekal <nithinbekal@gmail.com>
2016-04-27 23:48:18 +0530

    Authenticable#authenticate_with_token

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -3,4 +3,9 @@ module Authenticable
     @current_user ||=
       User.find_by(auth_token: request.headers['Authorization'])
   end
+
+  def authenticate_with_token
+    return if current_user.present?
+    render json: { errors: 'Not authenticated' }, status: 401
+  end
 end
--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -4,6 +4,7 @@ class Authentication
   include Authenticable
 
   def request; end
+  def response; end
 end
 
 describe Authenticable do
@@ -17,6 +18,19 @@ describe Authenticable do
         .and_return(request)
 
       expect(authentication.current_user.auth_token).to eq user.auth_token
+    end
+  end
+
+  describe '#authenticate_with_token' do
+    it 'renders json error message' do
+      allow(response).to receive(:body)
+        .and_return({'errors' => 'Not authenticated'}.to_json)
+
+      allow(authentication).to receive(:response).and_return(response)
+      allow(authentication).to receive(:request).and_return(nil)
+
+      res = JSON.parse(response.body)
+      expect(res['errors']).to eq 'Not authenticated'
     end
   end
 end
