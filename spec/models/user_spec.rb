2b8f227307ff0efd9abd8edaacd5acd9261bf5c6
Nithin Bekal <nithinbekal@gmail.com>
2016-04-27 18:44:30 +0530

    Add auth token to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  describe '#generate_authentication_token!' do
+    let(:user) { FactoryGirl.build(:user) }
+
+    it 'sets a unique token' do
+      expect(Devise).to receive(:friendly_token).and_return('auniquetoken')
+      user.generate_authentication_token!
+      expect(user.auth_token).to eq 'auniquetoken'
+    end
+  end
 end
