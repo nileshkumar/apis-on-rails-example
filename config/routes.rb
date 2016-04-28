fcdb5977b65b7f1b5b632304fe0c05a0cf31516b
Nithin Bekal <nithinbekal@gmail.com>
2016-04-28 23:34:03 +0530

    Add ProductsController#create, #upadte, #destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
-      resources :products, only: [:index, :show]
+      resources :products, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
