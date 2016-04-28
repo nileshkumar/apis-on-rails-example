512a6ec7e554e3232902aacfac276e4b82e4b49c
Nithin Bekal <nithinbekal@gmail.com>
2016-04-29 00:26:28 +0530

    Embed user JSON in product info

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -8,9 +8,13 @@ describe Api::V1::ProductsController, type: :controller do
       2.times { |i| Product.create(title: "Product #{i}", price: 200, user: user) }
       get :index
 
-      json = JSON.parse(response.body)
-      expect(json['products'].length).to be 2
+      products = JSON.parse(response.body).fetch('products')
+      expect(products.length).to be 2
       expect(response.status).to eq 200
+
+      first_product = products.fetch(0)
+      user_json = first_product.fetch('user')
+      expect(user_json['email']).to eq user.email
     end
   end
 
@@ -23,6 +27,9 @@ describe Api::V1::ProductsController, type: :controller do
       res = JSON.parse(response.body).fetch('product')
       expect(res['title']).to eq 'Product 42'
       expect(response.status).to eq 200
+
+      user_json = res.fetch('user')
+      expect(user_json['email']).to eq user.email
     end
   end
 end
